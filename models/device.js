0aee1878933e1bbff2e2b5cf25f20e5cf517b57b
Anmol Vijayvargiya <anmol.vijay94@gmail.com>
2017-05-23 11:57:10 -0700

    User and Device Schema Updated

--- models/device.js
+++ models/device.js
@@ -4,32 +4,18 @@ var Schema = mongoose.Schema;
 
 var DeviceSchema = new Schema(
 	{
-		name: {
-			type:String},
-
-		deviceid: {
-			type: Number,default:11},
-
-		image_url: {
-			type: String},
-
-		last_used: {
-			type: Date},
-
-		consumed_units: {
-			type:Number, default: 0},
-
-		scheduled: {
-			type: Boolean, default: false},
-
-		start: {
-			type:Date},
-
-		stop: {
-			type:Date}
-
+		name: 		{type:String},
+		did: 		{type: Number,unique: true},
+		uid: 		{type: Number, required:true},	
+		image_url: 	{type: String},
+		last_used: 	{type: Date},
+		consumed_units: {type:Number, default: 0},
+		schedule: 	{
+						isScheduled: {type: Boolean, default: false},
+						start: {type: Date},
+						stop: {type: Date}
+		}
 	});
 
-
+//Turned to a model before exporting
 module.exports = mongoose.model('Device', DeviceSchema);
-
--- models/user.js
+++ models/user.js
@@ -4,14 +4,21 @@ var Schema = mongoose.Schema;
 
 var UserSchema = Schema(
 	{
-		uid: {type: Number, required:true, unique: true},
-		name: {type:String, unique:true, required:true};
-		max_limit: 1000;
+		uid: 		{type: Number, required:true, unique: true},
+		name: 		{type:String, unique:true, required:true},
 		home_usage: {type:Number, default:0},
-		history_by_day: {type:Number, default:0},
-		history_by_week: {type:Number, default:0},
-		history_by_month: {type:Number, default:0}
+		history: {
+					day: 	{type:Number, default:0},
+					week: 	{type:Number, default:0},
+					month: 	{type:Number, default:0}
+				},
+		devices: [{
+					name:{type: String},
+					did: {type: Number}
+		 }],		
+		max_limit: 	1000,
 	});
 
 //Turned to a model before exporting
 module.exports = mongoose.model('User', UserSchema);
+		
